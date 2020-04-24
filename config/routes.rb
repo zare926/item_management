3a3f1eaf32a081a98b58451475318cb7cb88503f
zare926 <ilove.boc926@gmail.com>
2020-04-24 22:46:01 +0900

    topviewマークアップ5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index ,:new, :create]
   root to: 'items#index'
+  resources :items, only: [:index ,:new, :create]
+  resources :users, only: [:show]
 end
