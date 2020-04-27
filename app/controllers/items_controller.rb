1f8b938810ea59983190d8b0f2746ba836cd2551
zare926 <ilove.boc926@gmail.com>
2020-04-27 22:22:42 +0900

    registeritem_server1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
   end
 
   def edit
@@ -22,4 +23,9 @@ class ItemsController < ApplicationController
   def destroy
   end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:weather,:image,:text).merge(:user_id current_user.id)
+  end
 end
--- db/migrate/20200416235953_create_items.rb
+++ db/migrate/20200416235953_create_items.rb
@@ -1,12 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.integer :status
-      t.string :image
-      t.integer :limit
+      t.string :name, null: false
+      t.integer :status, default: 0
+      t.string :image, null: false
+      t.integer :limit, default: 4320
       t.integer :weather
       t.string :text
+      t.integer :user_id, null: false
       t.timestamps
     end
   end
