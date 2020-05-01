13d9c1dcfccced18d2bf51a096b87cbd0fea1372
zare926 <ilove.boc926@gmail.com>
2020-05-01 22:49:38 +0900

    itemeditマークアップ&サーバーサイド

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :timepass,only:[:index]
-  before_action :set_params,only:[:show]
+  before_action :set_params,only:[:show,:edit]
   def index
     if user_signed_in?
       @items = Item.where(user_id: current_user.id).order("created_at DESC")
@@ -26,6 +26,11 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    if item.save
+      redirect_to root_path and return
+    else
+      redirect_to new_item_path and return
+    end
   end
 
   def show
