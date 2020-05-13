45c698cfb93a6d070284e182dcc4802e91700056
zare926 <ilove.boc926@gmail.com>
2020-05-14 01:23:51 +0900

    deproy_Fix3

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,13 +33,6 @@ namespace :deploy do
   end
 end
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
-
 	
 # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
