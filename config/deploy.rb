849b46034839d3792b117d3bd5745f7a69451dce
zare926 <ilove.boc926@gmail.com>
2020-05-16 21:46:46 +0900

    deploy_Fix28

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,9 @@ set :linked_files, %w{ config/credentials.yml.enc }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+
   desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
