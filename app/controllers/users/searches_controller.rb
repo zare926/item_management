7c42a724e3e3869c01c97dffb33e692de8fc0a70
zare926 <ilove.boc926@gmail.com>
2020-05-19 23:22:48 +0900

    kaminariAdd

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,6 +1,6 @@
 class Users::SearchesController < ApplicationController
   def index
-    @users = User.search(params[:keyword])
+    @users = User.search(params[:keyword]).page(params[:page]).per(5)
     if user_signed_in?
       @items = Item.where(user_id: current_user.id).order("created_at DESC")
       @items_favorite = Item.where(favorite: 1)
